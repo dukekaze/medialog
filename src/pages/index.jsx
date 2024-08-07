3cb186ffa7c0d47b890131717eced1f24655e066
dukekaze <dukekaze77@gmail.com>
2024-08-07 12:30:06 +0800

    kk

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,32 @@
-import Navbar from "@/components/navbar";
 import Content from "@/components/content-section";
 import { Inter } from "next/font/google";
 import Trending from "@/components/trending";
-import TrendingRow from "@/components/trendingRow";
 import BlogPost from "@/components/blog-post";
-import SinglePost from "@/components/single-post";
 import LoadMore from "@/components/load-more";
-import Footer from "@/components/footer";
+import { useEffect, useState } from "react";
+import Card from "@/components/Card";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [articles, setArticles] = useState([]);
+
+  const getArticleData = async () => {
+    const response = await fetch("https://dev.to/api/articles");
+    const data = await response.json();
+    setArticles(data);
+  };
+
+  useEffect(() => {
+    getArticleData();
+  }, []);
+
+  console.log("articles", articles);
   return (
     <main>
       <Content />
       <Trending />
-      <BlogPost />
+      <Card />
       <LoadMore />
     </main>
   );
