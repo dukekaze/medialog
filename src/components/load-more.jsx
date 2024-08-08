4ae7509305a1e5fe01090a3effa227ba8dbd8850
dukekaze <dukekaze77@gmail.com>
2024-08-08 12:20:16 +0800

    duusav

--- src/components/load-more.jsx
+++ src/components/load-more.jsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 const LoadMore = () => {
   return (
     <div>
       <div className="flex justify-center mt-[100px]">
-        <button className="text-[#696A75] border w-[123px] h-[48px] rounded-md">
-          Load More
-        </button>
+        <Link href="/blog">
+          <button className="text-[#696A75] border w-[123px] h-[48px] rounded-md">
+            Load More
+          </button>
+        </Link>
       </div>
       <br />
       <br />
